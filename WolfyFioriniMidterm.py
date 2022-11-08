a1ec4bc9ae4fe149d32d895da1151b0c059e1698
Wolfy319 <jonwolffiorini@gmail.com>
2022-11-08 15:55:12 -0700

    Added seed grouping and  centers

--- WolfyFioriniMidterm.py
+++ WolfyFioriniMidterm.py
@@ -1,21 +1,22 @@
 import matplotlib as plt
 import cv2
+import numpy as np
 
 # Count seeds
     # Threshold endosperm
 
-def threshold(image, lower_thresh, upper_thresh):
-  b, g, r = cv2.split(image)
-  r_low, g_low, b_low = lower_thresh
-  r_high, g_high, b_high  = upper_thresh
+# def threshold(image, lower_thresh, upper_thresh):
+#   b, g, r = cv2.split(image)
+#   r_low, g_low, b_low = lower_thresh
+#   r_high, g_high, b_high  = upper_thresh
 
-  # Define thresholds
-  mask =  (r > r_low) &  (r < r_high) & (g > g_low) & (g < g_high) & (b > b_low) & (b < b_high)
+#   # Define thresholds
+#   mask =  (r > r_low) &  (r < r_high) & (g > g_low) & (g < g_high) & (b > b_low) & (b < b_high)
           
-  # Apply mask
-  image = image * mask.reshape(mask.shape[0], mask.shape[1], 1)
-  _, result = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)
-  return result
+#   # Apply mask
+#   image = image * mask.reshape(mask.shape[0], mask.shape[1], 1)
+#   _, result = cv2.threshold(image, 0, 255, cv2.THRESH_BINARY)
+#   return result
 
 def isolate_shapes(image):
     kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
@@ -29,7 +30,7 @@ def isolate_shapes(image):
     return closed_im
 
 # Count endosperm, coleoptile, and roots
-def count_shapes(image, lower_thresh, upper_thresh):
+def get_endosperm_or_coleoptile(image, lower_thresh, upper_thresh):
     # Convert to RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Extract desired objects
@@ -40,14 +41,44 @@ def count_shapes(image, lower_thresh, upper_thresh):
     shape_im = isolate_shapes(median_im)
     # Find shape contours
     contours, _ = cv2.findContours(shape_im, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
+    # Find approximate center of each contour
+    centers = []
+    for contour in contours:
+        moments = cv2.moments(contour)
+        centerX = int(moments["m10"]/moments["m00"])
+        centerY = int(moments["m01"]/moments["m00"])
+        centers.append([centerX, centerY])
+    print(centers)
     cv2.imshow("thresh", thresh_im)
     cv2.imshow("median filtered", median_im)
     cv2.imshow("shapes", shape_im)
-
     # Count shapes
-    return len(contours)
+    return len(contours), centers
 
+# Find roots
+    # Grab touching endosperm
+def get_roots(image, endosperm_thresh, coleoptile_thresh):
+    edges = cv2.Canny(image, 255,255,None,3)
+    cv2.imshow("e", edges)
+# Group endosperm with it's corresponding coleoptile and roots
+def group_objects(seed_centers, sprout_centers, root_tips):
+    groupings = []
+    min_distance = 25
+    for seed in seed_centers:
+        seedX = seed[0]
+        seed_data = []
+        grouped_roots = []
+        seed_data.append(seed)
+        for sprout in sprout_centers:
+            sproutX = sprout[0]
+            if np.abs(seedX - sproutX) <= min_distance:
+                seed_data.append(sprout)
+        for root in root_tips: 
+            rootX = root[0]
+            if np.abs(seedX - rootX) <= min_distance:
+                grouped_roots.append(root)
+        seed_data.append(grouped_roots)
+        groupings.append(seed_data)
 
 def germination_rate(seed_data):
     num_seeds = len(seed_data)
@@ -63,15 +94,14 @@ def germination_rate(seed_data):
     
 # Calculate germination rate
 # Count roots
-# Count Coleoptiles
 
 
 original = cv2.imread("maize.jpeg")
 cv2.imshow("maize", original)
 
-num_endosperm = count_shapes(original, (185,125,0), (255,255,100))
-num_coleoptile = count_shapes(original, (125,155,10), (188,205,120))
-print("Number of endosperm: ", num_endosperm)
-print("Number of coleoptile: ", num_coleoptile)
-
+# num_endosperm, endosperm_centers = get_endosperm_or_coleoptile(original, (185,125,0), (255,255,100))
+# num_coleoptile, coleoptile_centers = count_shapes(original, (125,155,10), (188,205,120))
+# print("Number of endosperm: ", num_endosperm)
+# print("Number of coleoptile: ", num_coleoptile)
+get_roots(original, [(185,125,0), (255,255,100)],[(125,155,10), (188,205,120)])
 cv2.waitKey()
