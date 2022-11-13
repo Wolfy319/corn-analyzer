b59e5817d9feae8e473d602ca31d5c6c36c6decb
Wolfy Fiorini <jonwolffiorini@gmail.com>
2022-11-12 22:37:05 -0700

    finalized counting, refining root detection

--- WolfyFioriniMidterm.py
+++ WolfyFioriniMidterm.py
@@ -19,72 +19,98 @@ import numpy as np
 #   return result
 
 def isolate_shapes(image):
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10,10))
-    dil = cv2.morphologyEx(image, cv2.MORPH_DILATE, kernel)
-    closed_im = cv2.morphologyEx(dil, cv2.MORPH_CLOSE, kernel, iterations=2)
-    cv2.imshow("close", closed_im)
-    # opened_im = cv2.morphologyEx(closed_im, cv2.MORPH_OPEN, kernel)
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (5,5))
+    big_kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15,15))
 
-    # return opened_im
-    # return closed_im
-    return closed_im
+    closed_im = cv2.morphologyEx(image, cv2.MORPH_CLOSE, kernel, iterations=2)
+    dilate = cv2.morphologyEx(closed_im, cv2.MORPH_DILATE, kernel)
+    median = cv2.medianBlur(dilate, 11)
+    # median = cv2.medianBlur(median, 7)
+    open = cv2.morphologyEx(median, cv2.MORPH_OPEN, kernel)
+    closed_im2 = cv2.morphologyEx(open, cv2.MORPH_CLOSE, big_kernel, iterations=3)
+    cv2.imshow("close", closed_im)
+    cv2.imshow("dil", dilate)
+    cv2.imshow("median", median)
+    cv2.imshow("close2", closed_im2)
+    cv2.imshow("open", open)
+
+    return closed_im2
 
 def detect_shapes(image):
     # Find shape contours
     contours, _ = cv2.findContours(image, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
     # Find approximate center of each contour
     centers = []
+    final_contours = []
     for contour in contours:
         moments = cv2.moments(contour)
+        if moments["m00"] < 100:
+            continue
+        final_contours.append(contour)
         centerX = int(moments["m10"]/moments["m00"])
         centerY = int(moments["m01"]/moments["m00"])
         centers.append([centerX, centerY])
-    return len(contours), centers
+    return len(centers), centers, final_contours
 
 def remove_shapes(image, shape_im):
-    shape_im = cv2.morphologyEx(shape_im, cv2.MORPH_DILATE, np.ones((5,5)), iterations=2)
+    shape_im = cv2.morphologyEx(shape_im, cv2.MORPH_DILATE, np.ones((10,10)))
     mask = np.where(shape_im > 0)
 
     for i in range(len(mask[0])):
         image[mask[0],mask[1]] = 0
+    return image
 
 # Count endosperm, coleoptile, and roots
 def get_endosperm_or_coleoptile(image, lower_thresh, upper_thresh):
+    
     # Convert to RGB
     image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
     # Extract desired objects
     thresh_im = cv2.inRange(image, lower_thresh, upper_thresh)
     # Reduce noise
-    median_im = cv2.medianBlur(thresh_im, 5)
+    # median_im = cv2.medianBlur(thresh_im, 5)
     # Isolate regions
-    shape_im = isolate_shapes(median_im)
+    shape_im = isolate_shapes(thresh_im)
     
-    num_shapes, shape_centers = detect_shapes(shape_im)
+    num_shapes, shape_centers, contours = detect_shapes(shape_im)
 
     cv2.imshow("thresh", thresh_im)
-    cv2.imshow("median filtered", median_im)
+    # cv2.imshow("median filtered", median_im)
     cv2.imshow("shapes", shape_im)
     # Count shapes
-    return num_shapes, shape_centers, shape_im
+    return num_shapes, shape_centers, shape_im, contours
 
 
 # Find root
     # Grab touching endosperm
-def get_roots(image, endosperm_shapes):
+def get_roots(image, endosperm_shapes, endosperm_contours):
     image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-    rem = remove_shapes(image, endosperm_shapes)
-    endosperm_border = np.min(np.where(endosperm_shapes > 0)[0])
+    opened = cv2.morphologyEx(endosperm_shapes, cv2.MORPH_OPEN, np.ones((10,10)))
+    cv2.imshow("root open", opened)
+    endosperm_top = np.min(np.where(opened > 0)[0]) + 10
+    endosperm_bottom = np.max(np.where(opened > 0)[0])
+
+    no_endosperm = remove_shapes(image, endosperm_shapes)
+    # cv2.imshow("removed", no_endosperm)
+
     # soil_border = np.max(np.where(endosperm_shapes > 0)[0])
     # print(soil_border)
     # pruned_im = cv2.line(image, (0, soil_border), (image.shape[0],soil_border), color=1,thickness=3)
-    pruned_im = image[endosperm_border:,:]
-    cv2.imshow("removed", rem)
-    # edges = cv2.Canny(pruned_im, 255,255,None,3)
-    _, thresh = cv2.threshold(pruned_im, 90, 255, cv2.THRESH_BINARY)
-    cv2.imshow("e", thresh)
-    # filtered = cv2.medianBlur(edges, 3)
+
+    pruned_im = image[endosperm_bottom:,:]
+    av_kernel = (1/3)**2 * np.ones((3,3))
+    blur = cv2.filter2D(pruned_im, -1, av_kernel)
+    edges = cv2.Canny(blur, 0,255,None,3)
+    closed_edges = cv2.morphologyEx(edges, cv2.MORPH_CLOSE, np.ones((5,5)))
+    _, thresh_im = cv2.threshold(blur, 90, 255, cv2.THRESH_BINARY)
+    
+    # edges = cv2.Canny(median, 0,255)
+    cv2.imshow("e", thresh_im)
+
+    filtered = cv2.medianBlur(thresh_im, 5)
     # cv2.imshow("e", edges)
-    # cv2.imshow("e2", filtered)
+    cv2.imshow("e2", filtered)
+    cv2.imshow("e3", closed_edges)
 
 
 # Group endosperm with it's corresponding coleoptile and roots
@@ -129,9 +155,9 @@ roi = original[100:h-50, 50:w-50, :]
 cv2.imshow("maize", original)
 cv2.imshow("roi", roi)
 
-num_endosperm, endosperm_centers, endosperm_shapes = get_endosperm_or_coleoptile(roi, (185,125,0), (255,255,100))
-num_coleoptile, coleoptile_centers, coleoptile_shapes = get_endosperm_or_coleoptile(roi, (125,155,10), (188,205,120))
-# print("Number of endosperm: ", num_endosperm)
+num_endo, endo_centers, endo_shapes, endo_contours = get_endosperm_or_coleoptile(roi, (185,125,0), (255,255,100))
+# num_col, col_centers, _, col_contours = get_endosperm_or_coleoptile(roi, (125,155,10), (188,205,120))
+print("Number of endosperm: ", num_endo)
 # print("Number of coleoptile: ", num_coleoptile)
-get_roots(roi, endosperm_shapes)
+get_roots(roi, endo_shapes, endo_contours)
 cv2.waitKey()
